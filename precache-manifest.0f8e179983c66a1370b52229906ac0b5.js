c2a060f7cc303cf250446b89abb30c7bdd979204
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-09-21 16:56:00 -0700

    Updates

---
+++ precache-manifest.0f8e179983c66a1370b52229906ac0b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072e34b68cd849f6345f592bbd8c3a93",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "acf816cafcfd780b8437",
+    "url": "/game-of-life/static/css/main.f18e4df3.chunk.css"
+  },
+  {
+    "revision": "aa11dc40a5170d67723a",
+    "url": "/game-of-life/static/js/2.d4670a62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life/static/js/2.d4670a62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acf816cafcfd780b8437",
+    "url": "/game-of-life/static/js/main.90fcda8b.chunk.js"
+  },
+  {
+    "revision": "a6893dc5f1659096e143",
+    "url": "/game-of-life/static/js/runtime-main.1dafd470.js"
+  }
+]);
